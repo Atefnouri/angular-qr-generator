8b8e8bb4fa2613c8d0486ad2097f3c596ef1e722
Atefnouri <nouriatef.isa@gmail.com>
2022-10-16 11:44:44 +0100

    enable image download feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -19,7 +19,8 @@ import { MaterialModule } from './Material/Material.module';
     FormsModule,
     MatChipsModule,
     QRCodeModule,
-    MaterialModule
+    MaterialModule,
+
 
   ],
   selector: 'app-root',
@@ -44,6 +45,10 @@ export class AppComponent {
   public selectedTheme:string = "";
   public selectedColor:string = "";
   public qrCodeWidth:number = 256;
+  public elementType:QRCodeElementType = "canvas";
+  public imageSrc:string  = './assets/logo.png';
+  public imageHeight:number = 79;
+  public imageWidth:number = 79;
 
 constructor(private _snackBar: MatSnackBar){
 this.loadFromLocalStorage();
@@ -195,6 +200,56 @@ this.loadFromLocalStorage();
         return "#536dfe";
       }
 
+}
+
+
+saveAsImage(parent: any) {
+  let parentElement = null
+
+  if (this.elementType === "canvas") {
+    // fetches base 64 data from canvas
+    parentElement = parent.qrcElement.nativeElement
+      .querySelector("canvas")
+      .toDataURL("image/png")
+  } else if (this.elementType === "img" || this.elementType === "url") {
+    // fetches base 64 data from image
+    // parentElement contains the base64 encoded image src
+    // you might use to store somewhere
+    parentElement = parent.qrcElement.nativeElement.querySelector("img").src
+  } else {
+    alert("Set elementType to 'canvas', 'img' or 'url'.")
+  }
+
+  if (parentElement) {
+    // converts base 64 encoded image to blobData
+    let blobData = this.convertBase64ToBlob(parentElement)
+    // saves as image
+    const blob = new Blob([blobData], { type: "image/png" })
+    const url = window.URL.createObjectURL(blob)
+    const link = document.createElement("a")
+    link.href = url
+    // name of the file
+    link.download = "Qrcode"
+    link.click()
+  }
+}
+
+
+private convertBase64ToBlob(Base64Image: string) {
+  // split into two parts
+  const parts = Base64Image.split(";base64,")
+  // hold the content type
+  const imageType = parts[0].split(":")[1]
+  // decode base64 string
+  const decodedData = window.atob(parts[1])
+  // create unit8array of size same as row data length
+  const uInt8Array = new Uint8Array(decodedData.length)
+  // insert all character code into uint8array
+  for (let i = 0; i < decodedData.length; ++i) {
+    uInt8Array[i] = decodedData.charCodeAt(i)
+  }
+  // return blob image after conversion
+  return new Blob([uInt8Array], { type: imageType })
 }
 
 }
---
+++ src/app/overlay/app.overlay.component.ts
@@ -0,0 +1,13 @@
+
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-overlay',
+  templateUrl: './app.overlay.component.html',
+  styleUrls: ['./app.overlay.component.scss']
+})
+export class OverlayComponent {
+
+}
